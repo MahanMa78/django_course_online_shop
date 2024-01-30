15419c0d5c8c92f5fbbd206a29e1769219849362
MahanMa78 <mahan78ir@gmail.com>
2024-01-30 18:03:07 +0330

    add login and signup

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class SignUpPageTests(TestCase):
+    username = 'myusername'
+    email='myusername@gmail.com'
+
+    
